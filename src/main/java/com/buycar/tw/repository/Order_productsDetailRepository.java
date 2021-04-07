a6b2bd921485243aa3831c3a74e5aee8efb54b1e
iiileochang <71544467+iiileochang@users.noreply.github.com>
2021-04-07 23:04:38 +0800

    Delete src/main directory

--- src/main/java/com/buycar/tw/repository/Order_productsDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.buycar.tw.repository;
-
-import com.buycar.tw.model.entity.Order_productsDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Order_productsDetailRepository extends JpaRepository<Order_productsDetail, Integer> {
-}
